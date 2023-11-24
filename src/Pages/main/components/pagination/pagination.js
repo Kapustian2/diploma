78b8595e6ec96637afa2f820c090dd03c5fadcdb
Kapustian2 <123888044+Kapustian2@users.noreply.github.com>
2023-11-24 19:33:21 +0600

    Implement input for pagination

--- src/Pages/main/components/pagination/pagination.js
+++ src/Pages/main/components/pagination/pagination.js
@@ -1,17 +1,33 @@
 import styled from "styled-components";
-import { Button } from "../../../../components";
+import { Button, Input } from "../../../../components";
 
 const PaginationContainer = ({ className, page, lastPage, setPage }) => {
+  const handleChange = (e) => {
+    const newPage = parseInt(e.target.value);
+    if (newPage < lastPage) {
+      setPage(newPage);
+    }
+    setPage(lastPage);
+  };
+
   return (
     <div className={className}>
-      <div className="current-page">{page}</div>
       <div className="buttons">
         <Button disabled={page === 1} onClick={() => setPage(1)}>
-          1
+          {"<<"}
         </Button>
         <Button disabled={page === 1} onClick={() => setPage(page - 1)}>
           {"<"}
         </Button>
+        <Input
+          type="number"
+          className="current-page"
+          onChange={handleChange}
+          value={page}
+          borderRadius="10px"
+          width="80px"
+          height="40px"
+        />
         <Button disabled={page === lastPage} onClick={() => setPage(page + 1)}>
           {">"}
         </Button>
@@ -32,6 +48,9 @@ export const Pagination = styled(PaginationContainer)`
     display: flex;
     gap: 10px;
   }
-  .current-page {
+  input {
+    text-align: center;
+    background-color: #f91155;
+    color: #f2f5f7;
   }
 `;
